996baa98dd4dbe215a7237f7cf898105dae5c281
jonmenard <jonmenard@cmail.carleton.ca>
2022-08-22 20:33:32 -0400

    adding Car Detection

--- mlapi.py
+++ mlapi.py
@@ -1,21 +1,49 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-import pickle
-import pandas as pd
+from fastapi import FastAPI, Request, File, UploadFile
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+# from typing import List, Union
+# from pydantic import BaseModel
+# import pickle
+# import pandas as pd
+# import os
+import numpy as np
+from numpy import expand_dims,asarray
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from PIL import Image
+import shutil
+# from starlette.responses import StreamingResponse
+model = keras.models.load_model(".mdl_wts.hdf5")
+
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-class ScoringItem(BaseModel): 
-    YearsAtCompany: float #/ 1, // Float value 
-    EmployeeSatisfaction: float #0.01, // Float value 
-    Position:str # "Non-Manager", # Manager or Non-Manager
-    Salary: int #4.0 // Ordinal 1,2,3,4,5
 
-with open('rfmodel.pkl', 'rb') as f: 
-    model = pickle.load(f)
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post('/')
-async def scoring_endpoint(item:ScoringItem): 
-    df = pd.DataFrame([item.dict().values()], columns=item.dict().keys())
-    yhat = model.predict(df)
-    return {"prediction":int(yhat)}
+@app.post("/uploading")
+async def upload(file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        with open("images/" + file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    image = np.asarray(load_img("images/" + file.filename, target_size=(225, 400))).astype('float32') / 255
+    image = expand_dims(image, axis=0) 
+
+    score, acc = model.evaluate(image,np.array([0]),verbose=0, batch_size=1)
+    if(acc >= 0.99):
+        shutil.move("./images/" + file.filename, "images/Car/" + file.filename)
+        return {"car": True, "score": score, "accuracy" : acc}
+    shutil.move("./images/" + file.filename, "images/NoCar/" + file.filename)    
+    return {"car": False, "score": score, "accuracy" : acc}
