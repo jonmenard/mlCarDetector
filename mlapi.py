3321517e6ebfac3516a66fc3a39de3c2080fb947
jonmenard <jonmenard@cmail.carleton.ca>
2022-08-24 13:21:06 -0400

    new updated model

--- mlapi.py
+++ mlapi.py
@@ -8,6 +8,7 @@ from fastapi.templating import Jinja2Templates
 # import pandas as pd
 # import os
 import numpy as np
+import uuid
 from numpy import expand_dims,asarray
 import tensorflow as tf
 from tensorflow import keras
@@ -33,19 +34,23 @@ async def upload(file: UploadFile = File(...)):
     score = 0
     acc = 0
     car = False
-    try:
+    # try:
     
-        contents = file.file.read()
-        image = Image.open(io.BytesIO(contents)).resize((400,225), Image.Resampling.NEAREST)
-        image = np.asarray(image).astype('float32') / 255
-        image = expand_dims(image, axis=0) 
-        score, acc = model.evaluate(image,np.array([0]),verbose=0, batch_size=1)
-        print(acc)
-        if(acc >= 0.99):
-            car = True
-    except Exception:
-        return {"message": "There was an error uploading the file"}
-    finally:
-        file.file.close()
+    contents = file.file.read()
+    image = Image.open(io.BytesIO(contents)).resize((400,225), Image.Resampling.LANCZOS)
+    # image.save("images/car/" + str(uuid.uuid4()) + file.filename)
+    image = np.asarray(image).astype('float32') / 255
+    image = expand_dims(image, axis=0)
+    score = model(image) 
+    # print(score)
+    score, acc = model.evaluate(image,np.array([0]),verbose=0, batch_size=1)
+    # print(acc, score)
+    if(acc >= 0.99):
+        # print("car")
+        car = True
+    # except Exception:
+    # return {"message": "There was an error uploading the file"}
+    # finally:
+    file.file.close()
         
     return {"car": car, "score": score, "accuracy" : acc}
