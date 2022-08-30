7915befc18346d3ed86356a296a8aba8b3ebb69f
jonmenard <jonmenard@cmail.carleton.ca>
2022-08-30 16:25:17 -0400

    websocket support

--- application.py
+++ application.py
@@ -28,13 +28,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
-
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
